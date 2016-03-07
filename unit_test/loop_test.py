50404e1cc3dcbba0ef8f73525d73b1acf315ee4e
zhaohui8969 <natas_hw@163.com>
2016-03-07 23:54:01 +0800

    用BloomFilter（hash表）写了环路测试，通过

--- unit_test/loop_test.py
+++ unit_test/loop_test.py
@@ -2,5 +2,31 @@
 # 环路测试，因为在爬行的过程中不可避免的会出现环路（用户直接、间接互相关注），
 # 因此需要解决环路问题
 
+from pybloomfilter import BloomFilter
+
 __author__ = 'natas'
 
+DATA = {1: {2, 3},
+        2: {3, 4},
+        3: None,
+        4: {1}}
+
+bf = BloomFilter(10000000, 0.01, 'filter.bloom')
+
+
+def traver(point):
+    print(point)
+    bf.add(point)
+    if not DATA[point] is None:
+        for nextPoint in DATA[point]:
+            if nextPoint not in bf:
+                traver(nextPoint)
+
+
+def main():
+    pass
+    traver(1)
+
+
+if __name__ == '__main__':
+    main()
