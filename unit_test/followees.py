46225596248539bf60f710e5163d2844698beca1
zhaohui8969 <natas_hw@163.com>
2016-03-07 15:17:49 +0800

    为了获取到用户所有的关注者，分析页面的Ajax过程，采用了更科学的方式（封装了一个API）来获取内容

--- unit_test/followees.py
+++ unit_test/followees.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import requests
 import zhihuURL
+import json
 from bs4 import BeautifulSoup
 
 __author__ = 'natas'
@@ -22,11 +23,29 @@ class Zhihu:
         self._session = requests.session()
         self._session.cookies.update(self.coockiDic)
         # 设置http头部
-        self._session.headers.update({'X-Requested-With': 'XMLHttpRequest',
-                                      'Referer': 'http://www.zhihu.com',
-                                      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
-                                                    'rv:39.0) Gecko/20100101 Firefox/39.0',
-                                      'Host': 'www.zhihu.com'})
+        self.head = zhihuURL.DEAFULT_HEADER.copy()
+        self._session.headers.update(self.head)
+
+    def xsrf(self, soup):
+        """获取知乎的反xsrf参数
+
+        :return: xsrf
+        :rtype: str
+        """
+        return soup.find('input', attrs={'name': '_xsrf'})['value']
+
+    def hash_id(self, soup):
+        """获取作者的内部hash id
+
+        :return: 用户hash id
+        :rtype: str
+        """
+        div = soup.find('div', class_='zm-profile-header-op-btns')
+        if div is not None:
+            return div.button['data-id']
+        else:
+            ga = soup.find('script', attrs={'data-name': 'ga_vars'})
+            return json.loads(ga.text)['user_hash']
 
     def getfollowers(self, username):
         """
@@ -45,21 +64,35 @@ class Zhihu:
             f.write(html)
         # print(html)
         soup = BeautifulSoup(html, 'lxml')
-        # 过滤出内容
-        zm_profile_section_wrap = soup.find(attrs={'class': ['zm-profile-section-wrap']})
-        followerstag = zm_profile_section_wrap.findAll('h2')
-        if len(followerstag) > 0:
-            for i in followerstag:
-                username = i.a.get('title')
-                userurl = i.a.get('href')
-                print("%s\t%s" % (username, userurl))
-                followers[username] = userurl
+        # 过滤出内容，分析页面的Ajax过程重写了这个部分
+        hashid = self.hash_id(soup)
+        xsrfstr = self.xsrf(soup)
+        headers = dict(zhihuURL.DEAFULT_HEADER)
+        headers['Referer'] = url
+        params = {"order_by": "created", "offset": 0, "hash_id": hashid}
+        data = {'_xsrf': xsrfstr, 'method': 'next', 'params': ''}
+        gotten_date_num = 20
+        offset = 0
+        while gotten_date_num == 20:
+            params['offset'] = offset
+            data['params'] = json.dumps(params)
+            res = self._session.post(zhihuURL.API_More_Followers_URL, data=data, headers=headers)
+            json_data = res.json()
+            gotten_date_num = len(json_data['msg'])
+            offset += gotten_date_num
+            for html in json_data['msg']:
+                soup = BeautifulSoup(html, 'lxml')
+                h2 = soup.find('h2')
+                author_name = h2.a.text
+                author_url = h2.a['href']
+                followers[author_name] = {'author_url': author_url}
         return followers
 
 
 def main():
     zhobj = Zhihu()
-    testuser = u'littleviper'
+    # testuser = u'littleviper'
+    testuser = u'zhao-hui-36-5'
     followers = zhobj.getfollowers(testuser)
     print("followers count: %d" % (len(followers)))
 
--- unit_test/zhihuURL.py
+++ unit_test/zhihuURL.py
@@ -2,6 +2,8 @@ __author__ = 'natas'
 
 PEOPLE_URL = ['https://www.Zhihu.com/people/']
 
+zhihuURL = r'https://www.zhihu.com'
+
 
 def followees(uername):
     return PEOPLE_URL[0] + uername + '/followees'
@@ -9,3 +11,12 @@ def followees(uername):
 
 def folloers(username):
     return PEOPLE_URL[0] + username + '/followers'
+
+
+API_More_Followers_URL = zhihuURL + '/node/ProfileFollowersListV2'
+
+DEAFULT_HEADER = {'X-Requested-With': 'XMLHttpRequest',
+                  'Referer': 'http://www.zhihu.com',
+                  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
+                                'rv:39.0) Gecko/20100101 Firefox/39.0',
+                  'Host': 'www.zhihu.com'}
